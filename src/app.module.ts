5b23026f86b374c96d616091d146afe992fe7723
Joaquin (Pato) Decima <jdecima@vasak.net.ar>
2024-10-13 10:07:46 -0300

    feat: Agregar configuración de puerto en app.module.ts
    Se agregó la configuración de puerto en el archivo app.module.ts para permitir la personalización del puerto en el que se ejecuta la aplicación. Esto se logró importando y utilizando el módulo de configuración de NestJS y agregando la configuración de puerto en el archivo de configuración correspondiente.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import configuration from './configs/configuration';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+      load: [configuration],
     }),
   ],
   controllers: [AppController],
